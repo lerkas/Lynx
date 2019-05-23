e425e9c1b88cc022d1864e2409a90f327aabe971
lerkas <lerkas@gmail.com>
2019-05-24 01:14:41 +0300

    1

---
+++ 1/script.js
@@ -0,0 +1,5 @@
+function toCalculate() {
+	let num1 = parseFloat(document.getElementById("x").value);
+	let num2 = parseFloat(document.getElementById("y").value);
+	return (num1 + num2).toFixed(2);
+}
